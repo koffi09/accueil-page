782ee155e12dd9a5bd788dd65867244d3dfedc30
koffi09 <45895139+koffi09@users.noreply.github.com>
2019-02-04 21:46:06 +0100

    Add files via upload

---
+++ cours-renfo.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/style1.css">
+    <script src="js/jquery-1.11.1.js"> </script>
+    <script src="js/bootstrap.min.js"></script> 
+        <!-- police font-->
+       <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+       <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+     <title>Accueil</title>
+  </head>
+       
+       <body>
+            <header>
+                 <!--ENTETE TU SITE --->
+                 <div class="container-fluid">
+                     <div class="row">
+                         <div class=" col-xs-12 col-md-4  col-sm-4 b1">
+                          <h1> <a href="cours-renfo.php">CR3TL </a></h1>
+                      </div>
+                     
+                     <div class="col-xs-12 col-sm-4 col-md-4">
+                      <h3><b>Cours de renforcement<br> en  vidéo en ligne </b> </h3>   
+                </div>
+                  
+                  <div class="col-xs-12 col-sm-4 col-md-4 bg">
+           <h2> &nbsp &nbsp  <b>"Contact" <br>  +225 49249477 </b></h2>
+        </div> 
+    </div>
+</div>
+
+        <nav class="my_navbar">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-md-12 col-lg-12">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">
+          <span class="glyphicon glyphicon-home"> ACCEUIL</span>
+        </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#">FORUM</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">COURS<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="cours-3eme.php">NIVEAU 3ème</a></li>
+            <li><a href="cours-terminalA.php">NIVEAU Tle A1-A2</a></li>
+          <li><a href="cours-terminalc.php">NIVEAU TLE C-E</a></li>
+          <li><a href="cours-terminald.php">NIVEAU TLE D</a></li>
+          </ul>
+        </li>
+        <li><a href="#">INSCRIPTION</a></li>
+        <li><a href="#">CONNECTION</a></li>
+      </ul>
+     </div>
+    </div>
+  </div>
+</div>
+</nav>
+   </header>
+     <!--CARROUSEL--->             
+   <div class="container-fluid">
+  <div class="row">
+    
+    <div class="row">
+        <!-- The carousel -->
+        <div id="transition-timer-carousel" class="carousel slide transition-timer-carousel" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#transition-timer-carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#transition-timer-carousel" data-slide-to="1"></li>
+        <li data-target="#transition-timer-carousel" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+                    <img src="http://placehold.it/1200x400" />
+                    <div class="carousel-caption">
+                        <h1 class="carousel-caption-header">Slide 1</h1>
+                        <p class="carousel-caption-text hidden-sm hidden-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim aliquet rutrum. Praesent vitae ante in nisi condimentum egestas. Aliquam.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="http://placehold.it/1200x400/AAAAAA/888888" />
+                    <div class="carousel-caption">
+                        <h1 class="carousel-caption-header">Slide 2</h1>
+                        <p class="carousel-caption-text hidden-sm hidden-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim aliquet rutrum. Praesent vitae ante in nisi condimentum egestas. Aliquam.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="http://placehold.it/1200x400/888888/555555" />
+                    <div class="carousel-caption">
+                        <h1 class="carousel-caption-header">Slide 3</h1>
+                        <p class="carousel-caption-text hidden-sm hidden-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim aliquet rutrum. Praesent vitae ante in nisi condimentum egestas. Aliquam.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#transition-timer-carousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#transition-timer-carousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+            
+            <!-- Timer "progress bar" -->
+            <hr class="transition-timer-carousel-progress-bar animate" />
+    </div>
+    </div>
+</div>
+   </div>
+  </section> 
+  <!--FIN CROUSEL-->
+
+                  <div class="row">
+                     <div class="col-md-12">
+                    <div class="titre">
+                    <h1>  PREPARE TON EXAMEN A TOUS MOMENTS ET EN TOUTES SERENITES</h1>
+               </div>
+          </div>
+      </div>
+      <section id="contenu">
+               <div class="container"> 
+               <div class="row">
+                    <div class="col-xs-12">
+                      <div class="titre">
+                       <h3>  Des exercices corrigés uniquements en vidéo pour les classes <br>
+                      de troisième et terminal afin de mieux travailler et progresser toutes l'année. </h3>
+                    </div>
+                 </div>
+                </div>
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                           <div class="titre">
+                             <h2 style="color:#F9AD3B"> <b>Découvrez le CR3TL AVANT DE VOUS INSCIRE</b></h2>
+                        </div>
+                   </div>
+              </div>
+           </div>
+    </section>
+       
+
+
+       <section id="presentation">
+       <div class="box">
+    <div class="container">
+      <div class="row">
+       
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+          <div class="box-part text-center">
+                        
+                        <i class="fa fa-behance fa-3x" aria-hidden="true"></i>
+                        
+            <div class="title">
+              <h3 style="text-decoration:underline;color:#37BDBF"><b>NOS FORMATIONS</b></h3>
+            </div>
+                        
+            <div class="text">
+              <h4>Nos cours sont typiquement en vidéo afin de permettre aux eleves de mieux comprendres les chapitres.</h4>
+            </div>
+                        
+           </div>
+        </div>   
+        
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+          <div class="box-part text-center">
+              
+              <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    
+            <div class="title">
+              <h3 style="text-decoration: underline;color:#37BDBF"> <b>LES CLASSE D'EXAMEN</b></h3>
+            </div>
+                        
+            <div class="text">
+              <h4>Etre en 3eme ou en Terminal tu as surement   besoin 
+                       d'etre éfficace et rapide pour réussir tes devoirs et intérros 
+                       en classe et aussi à ton examen ? tu es le/la bienvenu sur le CR3TL</h4>    
+            </div>
+                      
+                        
+           </div>
+        </div>   
+        
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+          <div class="box-part text-center">
+                        
+                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        
+            <div class="title">
+              <h3 style="text-decoration: underline;color:#37BDBF"> <b>AIDES DEVOIRS MAISONS </b></h3>
+            </div>
+                        
+            <div class="text">
+              <h4> CR3TL vous aides dans vos devoirs de maison 
+                        en envoyant vos préocupation sur le groupe  Whattsapp CR3TL
+                        afin de vous  Aider</h4>
+            </div>
+                        
+           </div>
+        </div>    
+       </div>
+     </div>
+      </div>
+      </section>  
+
+            
+            <section id="section3">
+            <div class="container"style="background-color:#A9A9A9">
+                <div class="row">
+                   <div class="col-md-12 col-xs-12 col-lg-12">
+                       <div class="prix">
+                        <h1 style="color:#37BDBF">Accès en illimités des contenus</h1>
+                         <h3>  Avec le compte  CR3TL accedez aux cours en illimité à seulement <b>1000f/mois</b></h3>
+                         <h4> Accès en illimités des vidéo</h4>
+                         <h4>  Accès à l'aide aux devoirs de maison</h4>
+                         <h1> <button type="button" class="btn btn-circle btn-info"style="color:#6ED5D7"> <a href="#" style="text-decoration:none"><b>
+                         S'abonner</b> </a></button></h1>
+                          
+                         </div>
+                     </div>
+                   </div>
+                 </div>
+                
+                </section>
+              </div>
+<!---fin section ---->
+
+
+
+
+
+<!---- foooter----->
+
+<footer> <br> <br>
+  <div class="container-fluid footer">
+           <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12"> 
+              <div class="contep" style="margin-left:25px">
+              <h3> <strong> Niveau et matières</strong> </h3>
+        </div>
+           </div>
+            </div>
+            
+            <div class="row">
+               <div class="col-md-3 col-sm-3 col-xs-3">
+                <div class="contep" style="margin-left:25px">
+              <h5> Niveau 3eme </h5>
+              <ul>
+               <li><a href="#"> Mathématiques</a></li>
+               <li> <a href="#">  Physiques-chimies</a></li>  
+               <li>     <a href="#"> Anglais </a></li>
+                 <li>  <a href="#"> Francais </a></li>   
+                </ul>
+                </div>
+              </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+          <h5> Niveau Terminal C-E </h5>
+              <ul>
+               <li> <a href="#">Mathématiques</a></li>
+               <li> <a href="#"> Physiques-chimies </a></li>  
+               <li> <a href="#">Francais </a></li>
+                 <li> <a href="#"> SVT</a></li>   
+              </ul>
+        </div>
+         
+         <div class="col-md-3 col-sm-3 col-xs-3">
+          <h5> Niveau Terminal D </h5>
+              <ul>
+               <li> <a href="#">Mathématiques </a></li>
+               <li> <a href="#"> Physiques-chimies </a></li>  
+               <li> <a href="#">Francais </a></li>
+                 <li> <a href="#"> SVT </a></li>   
+              </ul>
+        </div>
+
+         <div class="col-md-3 col-sm-3 col-xs-3">
+          <h5> Niveau Terminal A</h5>
+              <ul>
+               <li> <a href="#">Philosophie</a></li>
+               <li> <a href="#"> Anglais </a></li>  
+               <li><a href="#"> Francais </a></li>
+                 <li> <a href="#">Allemand </a></li>   
+               <li><a href="#"> Espagnol </a> </li>   
+              </ul>
+        </div>
+             </div>
+
+                 <div class="row">
+           
+           <div class="contep" style="margin-left:25px">
+            <div class="col-md-4 col-sm-4 col-xs-12"> 
+              <h3> <strong> Aides</strong> </h3>
+              </div>
+            <div class="col-md-4 col-sm-4 col-xs-12"> 
+              <h3> <a href=""> Dévoirs-maison </a></h3>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12"> 
+              <h3> <a href=""> Cours-particuliers </a></h3>
+            </div>
+        </div>
+        </div>
+   <div class="row"> <hr>
+<div class="content" style="margin-left:25px">
+<div class="col-md-4 col-sm-4 col-xs-3">
+  <h3><a href="#">A propos de nous</a></h3>
+</div>
+
+<div class="col-md-4 col-sm-4 col-xs-4"> 
+<h3>   Nous contacter </h3>
+  <ul>
+  <li> <h4><a href="#"> <img src="image/facebook.png" class="img-reponsive" style="width:30px"> facebook </a></h4></li>
+<li><h4><a href="#"><img src="image/whatapp.png" style="width:30px"class="img-reponsive">Whatsapp +225 xx.xx.xx.xx </a></h4></li>
+  <li><h4><a href="#"><img src="image/gmail.png" style="width:30px" class="img-reponsive"> email:cr3tl@gmail.com </a></h4></li>
+
+  </ul>
+</div>
+
+<div class="col-md-4 col-sm-4 col-xs-4"> 
+<h3> <a href="" style="text-decoration:none">Récrutement de pof à domicil </a> </h3>
+  </div>
+     </div>
+      </div>
+
+<div class="row"> 
+    <div class="copiright">
+   <div class="col-md-12 col-sm-12 col-xs-12">
+   <h4 class="pull-left"> © CR3TL 2019 </h4>
+   </div>
+ </div>
+    </div>
+        </div> 
+      </div>
+    </footer>
+  
+
+
+  </body>
+</html>
+
+
+<!---javascript pour carousel  --->
+
+
+
+<script type="text/javascript">
+$(document).ready(function() {    
+    $("#transition-timer-carousel").on("slide.bs.carousel", function(event) {
+        $(".transition-timer-carousel-progress-bar", this)
+            .removeClass("animate").css("width", "0%");
+    }).on("slid.bs.carousel", function(event) {
+        $(".transition-timer-carousel-progress-bar", this)
+            .addClass("animate").css("width", "100%");
+    });
+    $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
+        .css("width", "100%");
+});
+
+</script>
+
+ 
+
